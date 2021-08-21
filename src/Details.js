98a7651cc268c81ea3d9be2b95eb483ac5562a34
Don Spire <don.spire1@gmail.com>
2021-08-20 20:50:59 -0700

    add Carousel images to Details comp

---
+++ src/Details.js
@@ -0,0 +1,46 @@
+import { Component } from "react";
+import { useParams, withRouter } from "react-router-dom";
+import Carousel from "./Carousel";
+
+class Details extends Component {
+  constructor() {
+    super();
+
+    this.state = { loading: true };
+  }
+
+  async componentDidMount() {
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/pets?id=${this.props.match.params.id}`
+    );
+    const json = await res.json();
+    this.setState(
+      Object.assign(
+        {
+          loading: false,
+        },
+        json.pets[0]
+      )
+    );
+  }
+
+  render() {
+    if (this.state.loading) {
+      return <h2>Loading...</h2>;
+    }
+
+    const { animal, breed, city, state, description, name, images } =
+      this.state;
+    return (
+      <div className="details">
+        <Carousel images={images} />
+        <h1>{name}</h1>
+        <h2>{`${animal} - ${breed} - ${city}, ${state} `}</h2>
+        <button>Adopt {name}</button>
+        <p>{description}</p>
+      </div>
+    );
+  }
+}
+
+export default withRouter(Details);
